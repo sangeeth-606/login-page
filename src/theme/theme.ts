f9ff629644a608c50dba87c85924fe0dc18e83cd
sangeeth911 <sangeeth999123@gmail.com>
2024-12-10 15:27:31 +0530

    dark as default

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,8 +1,8 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
-  useSystemColorMode: false,
+  initialColorMode: 'dark',
+  useSystemColorMode: false, // Ignores the system's color mode.
 };
 
 const theme = extendTheme({ 
